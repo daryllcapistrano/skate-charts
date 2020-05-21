8da4c471c45f0472e966dea9cfe6b8f10fe5b754
Daryll Capistrano <daryllcapistrano@gmail.com>
2020-05-20 19:16:31 -0700

    add top ten street data

---
+++ src/data/street-rankings.js
@@ -0,0 +1,165 @@
+export const maleRankings = [
+	{
+		rank: '1',
+		name: 'Nyjah Houston',
+		age: '25',
+		nation: 'United States',
+		wsrPoints: '145900',
+		stance: 'goofy'
+	},
+	{
+		rank: '2',
+		name: 'Yuto Horigome',
+		age: '21',
+		nation: 'Japan',
+		wsrPoints: '133200',
+		stance: 'goofy'
+	},
+	{
+		rank: '3',
+		name: 'Gustavo Ribeiro',
+		age: '19',
+		nation: 'Portugal',
+		wsrPoints: '87500',
+		stance: 'regular'
+	},
+	{
+		rank: '4',
+		name: 'Sora Shirai',
+		age: '18',
+		nation: 'Japan',
+		wsrPoints: '75060',
+		stance: 'goofy'
+	},
+	{
+		rank: '5',
+		name: 'Kelvin Hoefler',
+		age: '26',
+		nation: 'Brazil',
+		wsrPoints: '69660',
+		stance: 'goofy'
+	},
+	{
+		rank: '6',
+		name: 'Aurélien Giraud',
+		age: '22',
+		nation: 'France',
+		wsrPoints: '43330',
+		stance: 'goofy'
+	},
+	{
+		rank: '7',
+		name: 'Jake Ilardi',
+		age: '23',
+		nation: 'United States',
+		wsrPoints: '42640',
+		stance: 'regular'
+	},
+	{
+		rank: '8',
+		name: 'Maurio McCoy',
+		age: '24',
+		nation: 'United States',
+		wsrPoints: '39900',
+		stance: 'regular'
+	},
+	{
+		rank: '9',
+		name: 'Dashawn Jordan',
+		age: '23',
+		nation: 'United States',
+		wsrPoints: '37480',
+		stance: 'goofy'
+	},
+	{
+		rank: '10',
+		name: 'Jamie Foy',
+		age: '23',
+		nation: 'United States',
+		wsrPoints: '34570',
+		stance: 'regular'
+	}
+];
+
+export const femaleRankings = [
+	{
+		rank: '1',
+		name: 'Pamela Rosa',
+		age: '20',
+		nation: 'Brazil',
+		wsrPoints: '180000',
+		stance: 'regular'
+	},
+	{
+		rank: '2',
+		name: 'Rayssa Leal',
+		age: '12',
+		nation: 'Brazil',
+		wsrPoints: '148000',
+		stance: 'goofy'
+	},
+	{
+		rank: '3',
+		name: 'Aori Nishimura',
+		age: '18',
+		nation: 'Japan',
+		wsrPoints: '89600',
+		stance: 'regular'
+	},
+	{
+		rank: '4',
+		name: 'Leticia Bufoni',
+		age: '27',
+		nation: 'Brazil',
+		wsrPoints: '64000',
+		stance: 'goofy'
+	},
+	{
+		rank: '5',
+		name: 'Candy Jacobs',
+		age: '29',
+		nation: 'Netherlands',
+		wsrPoints: '56240',
+		stance: 'goofy'
+	},
+	{
+		rank: '6',
+		name: 'Hayley Wilson',
+		age: '18',
+		nation: 'Australia',
+		wsrPoints: '47500',
+		stance: 'goofy'
+	},
+	{
+		rank: '7',
+		name: 'Mariah Duran',
+		age: '23',
+		nation: 'United States',
+		wsrPoints: '46670',
+		stance: 'goofy'
+	},
+	{
+		rank: '8',
+		name: 'Gabriela Pereira Mazetto',
+		age: '22',
+		nation: 'Brazil',
+		wsrPoints: '42720',
+		stance: 'regular'
+	},
+	{
+		rank: '9',
+		name: 'Alexis Sablone',
+		age: '33',
+		nation: 'United States',
+		wsrPoints: '35730',
+		stance: 'goofy'
+	},
+	{
+		rank: '10',
+		name: 'Virginia Fortes Aguas',
+		age: '13',
+		nation: 'Brazil',
+		wsrPoints: '35130',
+		stance: 'regular'
+	}
+];
